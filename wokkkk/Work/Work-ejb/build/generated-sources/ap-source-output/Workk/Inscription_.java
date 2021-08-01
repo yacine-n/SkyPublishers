b2008bffcf2b8c4e0ed4e06da8482da75000c19c
Yacine Ndiaye <65970214+yacine-n@users.noreply.github.com>
2021-08-01 23:18:35 +0200

    Add files via upload

---
+++ wokkkk/Work/Work-ejb/build/generated-sources/ap-source-output/Workk/Inscription_.java
@@ -0,0 +1,17 @@
+package Workk;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-01T16:03:37")
+@StaticMetamodel(Inscription.class)
+public class Inscription_ { 
+
+    public static volatile SingularAttribute<Inscription, String> mail;
+    public static volatile SingularAttribute<Inscription, String> confmtp;
+    public static volatile SingularAttribute<Inscription, Long> id;
+    public static volatile SingularAttribute<Inscription, String> nom;
+    public static volatile SingularAttribute<Inscription, String> motp;
+
+}
