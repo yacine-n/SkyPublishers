c119217084a19502159594aeba69c7f1a5d18d85
Yacine Ndiaye <65970214+yacine-n@users.noreply.github.com>
2021-08-01 23:17:44 +0200

    Add files via upload

---
+++ wokkkk/Work/Work-ejb/build/generated-sources/ap-source-output/Work/FormPub_.java
@@ -0,0 +1,26 @@
+package Work;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-06-01T16:03:37")
+@StaticMetamodel(FormPub.class)
+public class FormPub_ { 
+
+    public static volatile SingularAttribute<FormPub, String> resume;
+    public static volatile SingularAttribute<FormPub, String> fichier;
+    public static volatile SingularAttribute<FormPub, String> ville;
+    public static volatile SingularAttribute<FormPub, String> codep;
+    public static volatile SingularAttribute<FormPub, String> titre;
+    public static volatile SingularAttribute<FormPub, String> type;
+    public static volatile SingularAttribute<FormPub, String> nom;
+    public static volatile SingularAttribute<FormPub, String> edt;
+    public static volatile SingularAttribute<FormPub, String> adresse;
+    public static volatile SingularAttribute<FormPub, Integer> tel;
+    public static volatile SingularAttribute<FormPub, Long> id;
+    public static volatile SingularAttribute<FormPub, String> email;
+    public static volatile SingularAttribute<FormPub, String> info;
+    public static volatile SingularAttribute<FormPub, String> pays;
+
+}
